530ed9b0d4485d0685538b61b4f158e64ad50d55
alengcm <alengcm@gmail.com>
2020-10-28 18:47:55 +1030

    f]

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Graph from "react-graph-vis";
+// import Graph from "../../lib";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// import Graph from 'react-graph-vis'
+
+import React from "react";
+import { render } from "react-dom";
+ 
+
+const graph = {
+  nodes: [
+    { id: 1, label: "Node 1", color: "#e04141" },
+    { id: 2, label: "Node 2", color: "#e09c41" },
+    { id: 3, label: "Node 3", color: "#e0df41" },
+    { id: 4, label: "Node 4", color: "#7be041" },
+    { id: 5, label: "Node 5", color: "#41e0c9" }
+  ],
+  edges: [{ from: 1, to: 2, label: "pling" }, { from: 1, to: 3 }, { from: 2, to: 4 }, { from: 2, to: 5 }]
+};
+
+const options = {
+  layout: {
+    hierarchical: false
+  },
+  edges: {
+    color: "#000000"
+  },
+  height: "650px"
+};
+ 
+
+render(
+  <div>
+ 
+    <Graph graph={graph} options={options}     />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
